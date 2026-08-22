cc84952388b234f29bfcbeec9adb53e723644a8d
Himani Garkoti <garkotihimani2005@gmail.com>
2026-08-22 10:42:51 +0530

    Setup MongoDB connection using Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
-import app from './app.js';
+import dotenv from 'dotenv'
+dotenv.config();
 
+import app from './app.js';
+import connectDb from './db/db.js';
+
+connectDb();
 
 app.listen(3000,()=>{
     console.log("Server is connected at 3000 Port");
